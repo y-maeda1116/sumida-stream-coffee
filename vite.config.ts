8a447b46316b053ca24e9b66e9f6156720a904bc
y-maeda1116 <yasuyuki.ma3da@gmail.com>
2026-05-09 23:50:00 +0900

    fix: set base to / for custom domain ink.maeda.coffee

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
-  base: '/sumida-stream-coffee/',
+  base: '/',
   build: {
     target: 'esnext',
   },
